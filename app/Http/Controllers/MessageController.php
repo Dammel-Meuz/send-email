92ccee2444f7deb8f9d549d72242d44035ab83bd
Dammel-Meuz <ndoyemamemoussa@gmail.com>
2022-11-11 21:10:20 +0000

    premier commit

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Helper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+
+class MessageController extends Controller
+{
+    public function sendMail(Request $request){
+        $request->validate([
+            'name' =>'required',
+            'email' =>'required|email',
+            'subject'=>'required',
+            'message' =>'required'
+            
+        ]);
+
+        $path='/upload';
+        $newname=Helper::renameFile($path,$request->file('piecejoint')->getClientOriginalName());
+        $upload=$request->piecejoint->move(public_path($path),$newname);
+
+
+
+
+        if ($this->isOnline()) {
+            $mail_data=[
+                'recipient'=>'mamemoussandoye5@gmail.com',
+                'fromEmail'=>$request->email,
+                'formName'=>$request->name,
+                'subject'=>$request->subject,
+                'body'=>$request->message,
+                'piecejoint'=>$request->file('piecejoint')->getClientOriginalName()
+            ];
+
+          //  dd($mail_data);
+            // if (!preg_match("^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$",$mail_data['fromEmail'])) {
+            //     return redirect()->back()->withInput()->with('errormail','incorrect email address');
+            //   }
+        
+          
+            Mail::send('email-template',$mail_data,function($message)use($mail_data){
+                $message->to($mail_data['fromEmail'])
+                        ->from($mail_data['recipient'])
+                        ->subject($mail_data['subject'])
+                        ->attach(public_path('upload/'.$mail_data['piecejoint']));
+            });
+            return redirect()->back()->with('success', 'Email send successfully');
+        }else{
+            return redirect()->back()->withInput()->with('error','Check your internet connection');
+        }
+    }
+
+
+
+    public function isOnline($site="https://youtube.com/"){
+        if (@fopen($site,'r')) {
+
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+    <title>send-Email</title>
+</head>
+<body>
+    
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3 mt-5" >
+            <h4>Contact Us</h4><hr>
+            <form action="{{route('sendMail')}}" method="post" enctype="multipart/form-data">
+                @csrf
+
+                @if (Session::has('error'))
+                    <div class="alert alert-danger">
+                        {{Session::get('error')}}
+                    </div>
+                @endif
+
+                @if (Session::has('success'))
+                <div class="alert alert-success">
+                    {{Session::get('success')}}
+                </div>
+                    
+                @endif
+                @if (Session::has('error'))
+                <div class="alert alert-success">
+                    {{Session::get('errormail')}}
+                </div>
+                    
+                @endif
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Entrer your name" value="{{old('name')}}">
+                    @error('name')<span class="text-danger">{{$message}}</span>@enderror
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" name="email" placeholder="Entrer your email" value="{{old('email')}}">
+                    @error('email')<span class="text-danger">{{$message}}</span>@enderror
+                </div>
+                <div class="form-group">
+                    <label for="subject">Subject</label>
+                    <input type="text" class="form-control" name="subject" placeholder="Entrer your subject" value="{{old('subject')}}">
+                    @error('subject')<span class="text-danger">{{$message}}</span>@enderror
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea type="text" cols="4" rows="4" class="form-control" name="message" placeholder="Entrer your message" value="{{old('message')}}"></textarea>
+                    @error('name')<span class="text-danger">{{$message}}</span>@enderror
+                </div>
+                <div class="form-group">
+                    <label for="subject">Subject</label>
+                    <input type="file" class="form-control" name="piecejoint" placeholder="piecejoint" value="{{old('piecejoint')}}">
+                </div>
+                <button type="submit" class="btn btn-primary m-3" >Send</button>
+            </form>
+        </div>
+    </div>
+</div>
+    
+    
+
+
+</body>
+</html>
